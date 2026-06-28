79dcf28e4611f7ba72425b13d37b8bbe4bc6cf71
Axiom Coder <prettylioness0550@gmail.com>
2026-06-28 12:33:45 +0530

    Update settings.py

--- ScarletV5/plugins/bot/settings.py
+++ ScarletV5/plugins/bot/settings.py
@@ -103,7 +103,7 @@ async def settings_back_markup(client, CallbackQuery: CallbackQuery, _):
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
         media=InputMediaVideo(
-            media="https://files.catbox.moe/rxiwb3.mp4",
+            media="https://files.catbox.moe/2hbbqx.mp4",
             has_spoiler=True,
             caption="**ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ᴍᴜsɪᴄ ʙᴏᴛ ᴡᴀᴛᴄʜɪɴɢ ᴛʜᴇ ᴠɪᴅᴇᴏ ᴄᴀʀᴇғᴜʟʟʏ.**"
         ),
