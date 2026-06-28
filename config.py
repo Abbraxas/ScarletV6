23f6cf7d38fc68d6a625b531ccba10d09db522bf
Axiom Coder <prettylioness0550@gmail.com>
2026-06-28 11:00:43 +0530

    Files Uploaded By OwnerAxiom

---
+++ config.py
@@ -0,0 +1,118 @@
+import re
+from os import getenv
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_ID = getenv("BOT_ID")
+
+OWNER_USERNAME = getenv("OWNER_USERNAME", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "")
+BOT_NAME = getenv("BOT_NAME", "")
+ASSUSERNAME = getenv("ASSUSERNAME", "")
+
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://api01.shrutibots.site') ## 
+YT_API_KEY = getenv("YT_API_KEY" , 'ShrutiBotsiPkkfp6g9bwfEcUoNypL')
+
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
+
+LOGGER_ID = int(getenv("LOGGER_ID"))
+CLONE_LOGGER = LOGGER_ID
+
+OWNER_ID = int(getenv("OWNER_ID", 0))  # ✔️ default added
+
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/AxlommCoder7/ScarletV5.git",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+GIT_TOKEN = getenv("GIT_TOKEN", "")
+
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AxiomBots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Axlomm")
+GITHUB = getenv("GITHUB", "https://t.me/CreativeAxiom")
+
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
+AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "9000"))
+
+SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", "9999999"))
+SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "9999999"))
+
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "1c21247d714244ddbb09925dac565aed")
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "709e1a2969664491b58200860623ef19")
+
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+PLAYLIST_ID = -1001980154960  # Yahan apni ID dalein
+
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "5242880000"))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
+
+STRING1 = getenv("STRING_SESSION", "")
+STRING2 = getenv("STRING_SESSION2", None)
+
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+
+STREAMI_PICS = [
+    "https://files.catbox.moe/pljo17.png"
+]
+
+START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/pljo17.png")
+HELP_IMG_URL = getenv("HELP_IMG_URL", "https://files.catbox.moe/z2h5vg.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/bp9lgc.png")
+
+PLAYLIST_IMG_URL = "https://files.catbox.moe/pljo17.png"
+STATS_IMG_URL = "https://files.catbox.moe/pljo17.png"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/pljo17.png"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/pljo17.png"
+STREAM_IMG_URL = "https://files.catbox.moe/pljo17.png"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/pljo17.png"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/pljo17.png"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/pljo17.png"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/pljo17.png"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/pljo17.png"
+
+def time_to_seconds(time):
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(str(time).split(":"))))
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+
+if SUPPORT_CHANNEL and not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+    raise SystemExit("[ERROR] - SUPPORT_CHANNEL url must start with https://")
+
+if SUPPORT_CHAT and not re.match("(?:http|https)://", SUPPORT_CHAT):
+    raise SystemExit("[ERROR] - SUPPORT_CHAT url must start with https://")
+
+CMBOT = [
+    "💞", "🥂", "🔍", "🧪", "⚡️", "🔥", "🦋", "🎩", "🌈", "🍷",
+    "🥃", "🥤", "🕊️", "💌", "🧨", "✨", "💥", "💯", "🌟", "⚡️",
+
+    "❤️", "😍", "🥰", "😘", "😂", "🤣", "😱", "😡", "👏", "🙏",
+
+    "🎉", "🎊", "🎶", "🎵", "🎧", "🎸", "🎹", "🥁", "🎺", "🎷",
+
+    "🔥", "⚡️", "💫", "🌙", "☀️", "🌈", "❄️", "🌸", "🌺", "🌹",
+
+    "🦋", "🕊️", "🐍", "🐯", "🦁", "🐺", "🐉", "🦅", "🦄", "🐎"
+]
+
+EFFECT_ID = [
+    5046509860389126442,
+    5107584321108051014,
+    5104841245755180586,
+    5159385139981059251,
+]
