7d997d407958f87a26f7180fa6782bd4e184c68d
Axiom Coder <prettylioness0550@gmail.com>
2026-06-29 11:51:07 +0530

    Update bot.py

--- ScarletV5/core/bot.py
+++ ScarletV5/core/bot.py
@@ -28,7 +28,7 @@ class Axiom(Client):
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                text=f"<blockquote><b><u>✧ ᴛʜє ᴧxισϻ ᴄʟσηє ϻυsιᴄ ʙσᴛ sᴛᴧʀᴛєᴅ sυᴄᴄєssғυʟʟʏ.</u></b></blockquote><u>\n\n<blockquote expandable><b>✧ ηᴧϻє : {self.mention}\n✧ υsєʀηᴧϻє : @{self.username}\n✧ ɪᴅ : <code>{self.id}</code></b></blockquote>",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
