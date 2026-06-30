5f77714fc74330a67886d3e76e2e623ea0947c95
Axiom Coder <prettylioness0550@gmail.com>
2026-06-30 13:46:46 +0530

    Update help.py

--- ScarletV5/utils/inline/help.py
+++ ScarletV5/utils/inline/help.py
@@ -69,7 +69,7 @@ def private_help_panel(_):
             [
                 InlineKeyboardButton(
                     text=_["S_B_4"],
-                    url=f"https://t.me/{username}?start=help", 
+                    url=f"https://t.me/{app.username}?start=help", 
                     style=ButtonStyle.PRIMARY,
                 )
             ]
