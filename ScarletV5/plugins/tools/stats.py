de0e76d888a78debbbe4f265450e7ec1a9e47cd6
Axiom Coder <prettylioness0550@gmail.com>
2026-07-01 10:47:50 +0530

    Update stats.py

--- ScarletV5/plugins/tools/stats.py
+++ ScarletV5/plugins/tools/stats.py
@@ -6,7 +6,7 @@ from pyrogram import __version__ as pyrover
 from pyrogram import filters
 from pyrogram.errors import MessageIdInvalid
 from pyrogram.types import InputMediaPhoto, Message
-from pytgcalls.__version__ import __version__ as pytgver
+import pytgcalls; pytgver = pytgcalls.__version__
 
 import config
 from ScarletV5 import app
