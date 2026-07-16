5fd2ba4af6a854541382a13eb9e24b7b1876c1f4
Axiom Coder <prettylioness0550@gmail.com>
2026-07-16 12:08:47 +0530

    Update song.py

--- ScarletV5/utils/inline/song.py
+++ ScarletV5/utils/inline/song.py
@@ -1,22 +1,29 @@
 
 from pyrogram.types import InlineKeyboardButton
+from pyrogram.enums import ButtonStyle
 
+def random_style():
+    return random.choice([
+        ButtonStyle.SUCCESS,
+        ButtonStyle.DANGER,
+        ButtonStyle.PRIMARY
+    ])
 
 def song_markup(_, vidid):
     buttons = [
         [
             InlineKeyboardButton(
                 text=_["SG_B_2"],
-                callback_data=f"song_helper audio|{vidid}",
+                callback_data=f"song_helper audio|{vidid}", style=random_style(),
             ),
             InlineKeyboardButton(
                 text=_["SG_B_3"],
-                callback_data=f"song_helper video|{vidid}",
+                callback_data=f"song_helper video|{vidid}", style=random_style(),
             ),
         ],
         [
             InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"], callback_data="close"
+                text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.DANGER,
             ),
         ],
     ]
