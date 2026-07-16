e2bed16745e34141086d608683d75c06210fcc99
Axiom Coder <prettylioness0550@gmail.com>
2026-07-16 11:05:34 +0530

    Update stream.py

--- ScarletV5/utils/stream/stream.py
+++ ScarletV5/utils/stream/stream.py
@@ -105,7 +105,7 @@ async def stream(
                     photo=img,
                     has_spoiler=True,
                     caption=_["stream_1"].format(
-                        title[:23],
+                        title[:50],
                         f"https://t.me/{app.username}?start=info_{vidid}",
                         duration_min,
                         user_name,
@@ -167,7 +167,7 @@ async def stream(
             button = aq_markup(_, chat_id)
             await app.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name, f"https://t.me/{app.username}?start=info_{vidid}", user_id),
+                text=_["queue_4"].format(position, title[:50], duration_min, user_name, f"https://t.me/{app.username}?start=info_{vidid}", user_id),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
@@ -200,7 +200,7 @@ async def stream(
                 has_spoiler=True,
                 caption=_["stream_1"].format(
                     f"https://t.me/{app.username}?start=info_{vidid}",
-                    title[:23],
+                    title[:50],
                     duration_min,
                     user_name,
                     user_id,
@@ -229,7 +229,7 @@ async def stream(
             button = aq_markup(_, chat_id)
             await app.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name, f"https://t.me/{app.username}?start=info_{vidid}", user_id),
+                text=_["queue_4"].format(position, title[:50], duration_min, user_name, f"https://t.me/{app.username}?start=info_{vidid}", user_id),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
@@ -254,7 +254,7 @@ async def stream(
                 photo=config.SOUNCLOUD_IMG_URL,
                 has_spoiler=True,
                 caption=_["stream_1"].format(
-                    config.SUPPORT_CHAT, title[:23], duration_min, user_name, user_id
+                    config.SUPPORT_CHAT, title[:50], duration_min, user_name, user_id
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
@@ -282,7 +282,7 @@ async def stream(
             button = aq_markup(_, chat_id)
             await app.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name, f"https://t.me/{app.username}?start=info_{vidid}", user_id),
+                text=_["queue_4"].format(position, title[:50], duration_min, user_name, f"https://t.me/{app.username}?start=info_{vidid}", user_id),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
@@ -308,7 +308,7 @@ async def stream(
                 original_chat_id,
                 photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
                 has_spoiler=True,
-                caption=_["stream_1"].format(link, title[:23], duration_min, user_name, user_id),
+                caption=_["stream_1"].format(link, title[:50], duration_min, user_name, user_id),
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
@@ -336,7 +336,7 @@ async def stream(
             button = aq_markup(_, chat_id)
             await app.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name, f"https://t.me/{app.username}?start=info_{vidid}", user_id),
+                text=_["queue_4"].format(position, title[:50], duration_min, user_name, f"https://t.me/{app.username}?start=info_{vidid}", user_id),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
@@ -372,7 +372,7 @@ async def stream(
                 has_spoiler=True,
                 caption=_["stream_1"].format(
                     f"https://t.me/{app.username}?start=info_{vidid}",
-                    title[:23],
+                    title[:50],
                     duration_min,
                     user_name,
                     user_id,
@@ -399,7 +399,7 @@ async def stream(
             position = len(db.get(chat_id)) - 1
             button = aq_markup(_, chat_id)
             await mystic.edit_text(
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name, f"https://t.me/{app.username}?start=info_{vidid}", user_id),
+                text=_["queue_4"].format(position, title[:50], duration_min, user_name, f"https://t.me/{app.username}?start=info_{vidid}", user_id),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
