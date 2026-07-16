301ed4a43fa056a7566367e5a22153e1d50f8d4f
Axiom Coder <prettylioness0550@gmail.com>
2026-07-16 12:24:39 +0530

    Update extras.py

--- ScarletV5/utils/inline/extras.py
+++ ScarletV5/utils/inline/extras.py
@@ -1,13 +1,13 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import SUPPORT_CHAT
-
+from pyrogram.enums import ButtonStyle
 
 def botplaylist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT, style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.DANGER),
         ],
     ]
     return buttons
@@ -20,6 +20,7 @@ def close_markup(_):
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
+                    style=ButtonStyle.PRIMARY,
                 ),
             ]
         ]
@@ -34,6 +35,7 @@ def supp_markup(_):
                 InlineKeyboardButton(
                     text=_["S_B_9"],
                     url=SUPPORT_CHAT,
+                    style=ButtonStyle.SUCCESS,
                 ),
             ]
         ]
