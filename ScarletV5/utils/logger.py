3b1b96a5203bf3b7c26338ac4af19ee48f64d5e7
Axiom Coder <prettylioness0550@gmail.com>
2026-07-17 04:10:29 +0530

    Update logger.py

--- ScarletV5/utils/logger.py
+++ ScarletV5/utils/logger.py
@@ -1,4 +1,5 @@
 from pyrogram.enums import ParseMode
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from ScarletV5 import app
 from ScarletV5.utils.database import is_on_off
@@ -63,3 +64,57 @@ async def clone_bot_logs(client, message, bot_mention, clone_logger_id, streamty
             )
         except Exception as e:
             print(f"[ERROR] Clone Bot Log Failed ({bot_mention}): {e}")
+
+async def clone_main_logs(client, message, streamtype):
+
+    query = (
+        message.text.split(None, 1)[1]
+        if len(message.text.split(None, 1)) > 1
+        else "Unknown"
+    )
+
+    bot = await client.get_me()
+    bot_username = bot.username or "UnknownBot"
+
+    chat_username = message.chat.username
+    if chat_username:
+        chat_link = f"https://t.me/{chat_username}"
+    else:
+        chat_link = "Private Group"
+
+    user_link = f"tg://user?id={message.from_user.id}"
+
+    logger_text = f"""
+<blockquote><b>@{bot_username} ᴘʟᴀʏ ʟᴏɢ</b></blockquote>
+
+<blockquote><b>ᴄʜᴧᴛ ιᴅ :</b> <code>{message.chat.id}</code>
+<b>ᴄʜᴧᴛ ηᴧϻє :</b> {message.chat.title}
+<b>ᴄʜᴧᴛ ᴜsєꝛηᴧϻє :</b> @{message.chat.username}
+<b>ᴄʜᴀᴛ ʟɪɴᴋ :</b> {chat_link}</blockquote>
+
+<blockquote><b>ᴜsєꝛ ιᴅ :</b> <code>{message.from_user.id}</code>
+<b>ηᴧϻє :</b> {message.from_user.mention}
+<b>ᴜsєꝛηᴧϻє :</b> @{message.from_user.username}</blockquote>
+
+<blockquote><b>ǫᴜєꝛʏ :</b> {message.text.split(None, 1)[1]}
+<b>sᴛꝛєᴧϻᴛʏᴘє :</b> {streamtype}</blockquote>"""
+
+    buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("👤 Played By", url=user_link),
+                InlineKeyboardButton("💬 Open Group", url=chat_link if chat_username else user_link),
+            ]
+        ]
+    )
+
+    try:
+        await app.send_message(
+            chat_id=LOGGER_ID,
+            text=logger_text,
+            parse_mode=ParseMode.HTML,
+            reply_markup=buttons,
+            disable_web_page_preview=True,
+        )
+    except Exception as e:
+        print(e)
