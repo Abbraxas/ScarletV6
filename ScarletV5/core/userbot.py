569c9d34f0342bf6572e34e86030192822eff7f0
Axiom Coder <prettylioness0550@gmail.com>
2026-07-20 17:37:00 +0530

    Update userbot.py

--- ScarletV5/core/userbot.py
+++ ScarletV5/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Axlomm")
-                await self.one.join_chat("AxiomBots")
+                await self.one.join_chat("ScarletSupportChat")
+                await self.one.join_chat("ScarletUpdates")
             except:
                 pass
             assistants.append(1)
