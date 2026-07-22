d2ee002eb7c42f937ecca8c2c2f2b6c82b489706
Axiom Coder <prettylioness0550@gmail.com>
2026-07-22 16:42:14 +0530

    Update clone.py

--- ScarletV5/plugins/tools/clone.py
+++ ScarletV5/plugins/tools/clone.py
@@ -30,7 +30,7 @@ from datetime import datetime
 from pyrogram.types import ManagedBotUpdated
 CLONES = set()
 
-C_BOT_DESC = "𝐖‌єʟᴄσϻє ᴛσ ʏσυꝛ ᴘєꝛsσηᴧʟɪᴢєᴅ ϻυsɪᴄ 𝚺ᴄσsʏsᴛєϻ. \n\n𝐅‌ꝛσϻ sᴛꝛєᴧϻɪηɢ ᴧηᴅ ʙꝛσᴧᴅᴄᴧsᴛɪηɢ ᴛσ ᴘєꝛsσηᴧʟɪᴢєᴅ ϻєᴅɪᴧ, єᴠєꝛʏ ғєᴧᴛυꝛє ɪs ʙυɪʟᴛ ᴛσ ʙє ʏσυꝛs.\n\n𝐍‌єєᴅ ʏσυꝛ σᴡη? 𝐂‌ʟσηє ɪᴛ ɪη ᴧ ғєᴡ sєᴄσηᴅs ➜ @ScarletCloneBot\n\n• 𝐔‌ᴘᴅᴧᴛєs ➜ @ScarletUpdates\n• 𝐂‌ꝛєᴧᴛσꝛ ➜ @CreativeAxiom"
+C_BOT_DESC = "𝐖‌єʟᴄσϻє ᴛσ ʏσυꝛ ᴘєꝛsσηᴧʟɪᴢєᴅ ϻυsɪᴄ 𝚺ᴄσsʏsᴛєϻ. \n\n𝐅‌ꝛσϻ sᴛꝛєᴧϻɪηɢ ᴧηᴅ ʙꝛσᴧᴅᴄᴧsᴛɪηɢ ᴛσ ᴘєꝛsσηᴧʟɪᴢєᴅ ϻєᴅɪᴧ, єᴠєꝛʏ ғєᴧᴛυꝛє ɪs ʙυɪʟᴛ ᴛσ ʙє ʏσυꝛs.\n\n𝐍‌єєᴅ ʏσυꝛ σᴡη? 𝐂‌ʟσηє ɪᴛ ɪη ᴧ ғєᴡ sєᴄσηᴅs ➜ @cuqebot\n\n• 𝐔‌ᴘᴅᴧᴛєs ➜ @ScarletUpdates\n• 𝐂‌ꝛєᴧᴛσꝛ ➜ @CreativeAxiom"
 
 BOT_DP = "ScarletV5/assets/cloned_dp.jpg"
 
