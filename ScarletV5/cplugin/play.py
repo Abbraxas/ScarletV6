6b65140b3fcc6a3d3500059fdc04184ac67eb9a3
Dev-Naman <anshubairagi09@gmail.com>
2026-07-22 17:53:16 +0530

    Update play.py

--- ScarletV5/cplugin/play.py
+++ ScarletV5/cplugin/play.py
@@ -998,7 +998,7 @@ async def stream(
             button = aq_markup(_, chat_id)
             await client.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:18], duration_min, user_name, user_id, message.from_user.id),
+                text=_["queue_4"].format(position, title[:18], duration_min, user_name, f"https://t.me/{a.username}?start=info_{vidid}", user_id),
                 reply_markup=InlineKeyboardMarkup(button),
                 disable_web_page_preview=True,
             )
@@ -1062,7 +1062,7 @@ async def stream(
             button = aq_markup(_, chat_id)
             await client.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:18], duration_min, user_name, user_id, message.from_user.id),
+                text=_["queue_4"].format(position, title[:18], duration_min, user_name, C_SUPPORT_CHAT, user_id),
                 reply_markup=InlineKeyboardMarkup(button),
                 disable_web_page_preview=True,
             )
@@ -1115,7 +1115,7 @@ async def stream(
             button = aq_markup(_, chat_id)
             await client.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:18], duration_min, user_name, user_id, message.from_user.id),
+                text=_["queue_4"].format(position, title[:18], duration_min, user_name, f"https://t.me/{a.username}?start=info_{streamtype}", user_id),
                 reply_markup=InlineKeyboardMarkup(button),
                 disable_web_page_preview=True,
             )
@@ -1169,7 +1169,7 @@ async def stream(
             button = aq_markup(_, chat_id)
             await client.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:18], duration_min, user_name, user_id, message.from_user.id),
+                text=_["queue_4"].format(position, title[:18], duration_min, user_name, f"https://t.me/{a.username}?start=info_{vidid}", user_id),
                 reply_markup=InlineKeyboardMarkup(button),
                 disable_web_page_preview=True,
             )
@@ -1258,7 +1258,7 @@ async def stream(
             run = await client.send_photo(
                 original_chat_id,
                 photo=config.STREAM_IMG_URL,
-                caption=_["stream_2"].format(user_name, user_id, message.from_user.id),
+                caption=_["stream_2"].format(user_name),
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
