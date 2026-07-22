974ae022db025073c1f484ded7e79ba40d0578ea
Axiom Coder <prettylioness0550@gmail.com>
2026-07-22 18:21:16 +0530

    Update play.py

--- ScarletV5/utils/inline/play.py
+++ ScarletV5/utils/inline/play.py
@@ -76,13 +76,6 @@ def stream_markup_timer(_, chat_id, played, dur):
 
     buttons = [
         [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-                style=bar_style,
-            )
-        ],
-        [
             
             InlineKeyboardButton(text="ᴘᴧυsє", callback_data=f"ADMIN Pause|{chat_id}", style=random_style()),
             InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}", style=random_style()),
