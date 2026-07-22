8609559cf8f2baecaac1131fe01b33442ec4ba11
Axiom Coder <prettylioness0550@gmail.com>
2026-07-22 18:08:20 +0530

    Update play.py

--- ScarletV5/utils/inline/play.py
+++ ScarletV5/utils/inline/play.py
@@ -76,6 +76,13 @@ def stream_markup_timer(_, chat_id, played, dur):
 
     buttons = [
         [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+                style=bar_style,
+            )
+        ],
+        [
             
             InlineKeyboardButton(text="ᴘᴧυsє", callback_data=f"ADMIN Pause|{chat_id}", style=random_style()),
             InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}", style=random_style()),
@@ -84,8 +91,8 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(text="ᴧᴅᴅ ϻєєʜ", url=f"https://t.me/{app.username}?startgroup=true", style=bar_style),
-            InlineKeyboardButton(text="ꝛєᴘʟᴧʏ", callback_data=f"ADMIN Replay|{chat_id}", style=random_style()),
             InlineKeyboardButton(text="ᴄʟσsє", callback_data="close", style=ButtonStyle.DANGER),
+            InlineKeyboardButton(text="ηєᴡʙσᴛ", url=f"https://t.me/newbot/cuqebot", style=random_style()),
         ]
     ]
     return buttons
