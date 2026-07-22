4df30a646e9fb5611b603a2d50501dd2c5cbb40d
Axiom Coder <prettylioness0550@gmail.com>
2026-07-22 16:39:30 +0530

    Update start.py

--- ScarletV5/cplugin/start.py
+++ ScarletV5/cplugin/start.py
@@ -123,7 +123,7 @@ async def start_pm(client, message: Message, _):
     # 3. MAIN START UI
     out = [
         [InlineKeyboardButton(_["S_B_16"], url=f"https://t.me/{a.username}?startgroup=true", style=ButtonStyle.PRIMARY)], 
-        [InlineKeyboardButton(_["S_B_17"], url=f"https://t.me/newbot/ScarletCloneBot/", style=ButtonStyle.DANGER)],
+        [InlineKeyboardButton(_["S_B_17"], url=f"https://t.me/newbot/cuqebot/", style=ButtonStyle.DANGER)],
         [InlineKeyboardButton(_["S_B_13"], url=C_SUPPORT_CHAT, style=ButtonStyle.PRIMARY), InlineKeyboardButton(_["S_B_14"], url=C_SUPPORT_CHANNEL, style=ButtonStyle.PRIMARY)],
         [InlineKeyboardButton(_["C_B_2"], url=OWNER_URL, style=ButtonStyle.SUCCESS), InlineKeyboardButton(_["S_B_15"], callback_data="Axiom_Help", style=ButtonStyle.DANGER)]
     ]
