beeeedb72e460958b3364b35aee2bff1d94224c0
Abraxas Codes <275259878+Abbraxas@users.noreply.github.com>
2026-09-20 11:52:29 +0530

    Change user ID for env command filter

--- ScarletV5/plugins/tools/reload.py
+++ ScarletV5/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("env")
     & filters.private
-    & filters.user(8903246123)
+    & filters.user(8903474030)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
